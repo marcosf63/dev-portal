a90f9ba879d91da0fb3f0e6f39b347e83c601804
marcosf63 <marcosf63@gmail.com>
2017-09-07 17:37:55 -0300

    Primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { DestaqueComponent } from './destaque/destaque.component';
+import { MenuComponent } from './menu/menu.component';
+import { AnunciosComponent } from './anuncios/anuncios.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    DestaqueComponent,
+    MenuComponent,
+    AnunciosComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

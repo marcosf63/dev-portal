9526c3a8471f61adfcf03e1328bac77ebe06da87
U-RF03\23276380382 <23276380382@RF0310300022420.rf03.srf>
2017-10-06 16:53:27 -0300

    update

--- src/app/componentes/cadastro/cadastro.component.ts
+++ src/app/componentes/cadastro/cadastro.component.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from '../../modelos/categoria.model';
+import { Servico } from '../../modelos/servico.model';
+import { CategoriaService } from '../../servicos/categoria.service';
+import { ServicoService } from '../../servicos/servico.service'
+
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
+  providers: [ CategoriaService, ServicoService ]
 })
 export class CadastroComponent implements OnInit {
   etapaAtual: number = 1
   etapaAnterior: number = 0
   proximaEtapa: number = 2
 
-  constructor() { }
+  categorias: Categoria[] = []
+  servicos: Servico[] = []
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private servicoService: ServicoService,
+  ) { }
 
   ngOnInit() {
+    this.categoriaService.getCategorias()
+      .then(
+        (resposta: any) => {this.categorias = resposta}
+      );
+
+    
+  }
+  
+  onChangeSelectCategoria(valor) {
+    this.servicoService.getSevicoPorCategoria(valor)
+      .then(           
+        (resposta: any) => {
+          this.servicos = resposta
+        }
+      )
+
   }
 
   proximo() {

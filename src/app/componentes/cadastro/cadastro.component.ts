5785d0c7841ba0f2a8c841e6f4095c7e5f6c8e0e
marcosf63 <marcosf63@gmail.com>
2017-10-05 21:52:54 -0300

    Update

--- src/app/componentes/cadastro/cadastro.component.ts
+++ src/app/componentes/cadastro/cadastro.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
+  etapaAtual: number = 1
+  etapaAnterior: number = 0
+  proximaEtapa: number = 2
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  proximo() {
+    if (this.etapaAtual !== 4) {
+      this.etapaAnterior = this.etapaAtual
+      this.etapaAtual++
+
+    }
+  }
+
+  anterior() {
+    if (this.etapaAtual !== 0 ) {
+      this.etapaAtual--
+      this.etapaAnterior = this.etapaAtual - 1
+      
+    }
   }
 
 }
